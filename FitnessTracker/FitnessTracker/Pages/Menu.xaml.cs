74e0fec0c59710c2dff319f93842e040579778f0
AS-2020 <armin_schmidt_hn@web.de>
2020-11-22 12:15:43 +0100

    initial commit

---
+++ FitnessTracker/FitnessTracker/Pages/Menu.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FitnessTracker.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Menu : ContentPage
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void NewEntry_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new NewEntry());
+        }
+    }
+}
---
+++ FitnessTracker/FitnessTracker/Pages/NewEntry.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FitnessTracker.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NewEntry : ContentPage
+    {
+        public NewEntry()
+        {
+            InitializeComponent();
+        }
+
+        private void NewEntryBodyWeight_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new NewEntryBodyWeight());
+        }
+    }
+}
